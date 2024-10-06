0e466750712b6774a790830fcdc54d75a93ae282
MirellaRP <68485108+MirellaRP@users.noreply.github.com>
2024-10-06 18:48:18 -0500

    Update HashTableFinal.cpp
    ** HW3 Natalia, Angie, Mirella

--- HashTableFinal.cpp
+++ HashTableFinal.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <random>
 #include <set>
-#include <vector>
 #include <string>
 #include <type_traits>
 using namespace std;
@@ -12,177 +11,117 @@ using namespace std;
 template <typename T, typename V>
 struct Node {
     T key;
-    V value;      
+    V value;
     Node* next;
 };
 
-template <typename T,typename V>
+template <typename T, typename V>
 class LinkedList {
+    Node<T, V>* head;
 
-    //private:
-    Node<T,V>* head; // Atributo head
+public:
+    LinkedList() : head(NULL) {}
 
-    public:
-    
-    LinkedList() : head(NULL) {} // Inicializo el head como null 
-
-    int push_front(T key, V valor){ // Agrega un elemento al comienzo
-
-        Node<T,V>* temp = head;
-        bool found=false;
-        while(temp!=NULL && !found)
-        {
-            if(temp->key==key){
-                temp->value=valor;
-                found=true;
-            }else{
-                temp=temp->next;
+    int push_front(T key, V valor) {
+        Node<T, V>* temp = head;
+        bool found = false;
+        while (temp != NULL && !found) {
+            if (temp->key == key) {
+                temp->value = valor;
+                found = true;
+            } else {
+                temp = temp->next;
             }
         }
-        
-        if(found==true){
+
+        if (found) {
             return 0;
-        }else{
-            Node<T,V>* nodo = new Node<T,V>;
-            nodo->key=key;
-            nodo->value=valor;
-            nodo->next=head;
-            head=nodo;
+        } else {
+            Node<T, V>* nodo = new Node<T, V>;
+            nodo->key = key;
+            nodo->value = valor;
+            nodo->next = head;
+            head = nodo;
             return 1;
         }
-  
     }
 
-    T pop_front() {// Remueve el elemento al comienzo
-        if(head==NULL){ // Verificar si la lista esta vacia
-            return T();
-        }
-        else{
-            Node<T,V>* temp = head;
-            V valor = temp->value;
-            T key = temp->key;
-            head = head->next;
-            delete temp;
-            return key;
-        }
-    }
-
-    V findKey(T key){
-        Node<T,V>*  temp = head;
-        bool flag = false;
-        int i = 0;
-        while(!flag && temp!=NULL){
-            if(temp->key==key){
-                flag=true;
-            }else{
-                temp = temp->next;
-                i++;
+    V findKey(T key) {
+        Node<T, V>* temp = head;
+        while (temp != NULL) {
+            if (temp->key == key) {
+                return temp->value;
             }
-        }
-        
-        if(flag==true){
-            return temp->value;
-        }
-        else{
-            return V();
-        }
-
-    }
-
-    void deleteKey(T key){ //
-        Node<T,V>*  temp = head;
-        Node<T,V>* prev=NULL;
-        bool flag=false;
-
-        while(temp!=NULL && flag==false){
-            if(temp->key==key){
-                if(temp==head){
-                    head = head->next;
-                    delete temp;
-                }else if(temp->next==NULL){
-                    prev->next=NULL;
-                    delete temp;
-                }else{
-                    prev->next=temp->next;
-                    delete temp;
-                }
-                flag=true;
-            }else{
-                prev=temp;
-                temp=temp->next;
-            }
-        }
-        
-
-    }
-
-    T operator[ ](int valor){// Retorna el elemento en la posición indicada
-        Node<T,V>*  temp = head;
-        for(int i=0; i<=valor-1; i++){ //dependiendo si quiero iniciar con 0 o con 1 y puede ser acotado mejor con el size
             temp = temp->next;
-        } 
-        return temp->key;
-    }
-
-
-    bool empty(){// Retorna si la lista está vacía o no
-        if(head==NULL){ // Verificar si la lista esta vacia
-            return true;
-        }else{
-            return false;
         }
+        return V();
     }
 
-    int size(){// Retorna el tamaño de la lista
-        if(head==NULL){ // Verificar si la lista esta vacia
-            return 0;
-        } else{
-            int s = 1;
-            Node<T,V>* temp = head;
-            while (temp->next !=NULL){
-                temp = temp->next;
-                s++;
+    void deleteKey(T key) {
+        Node<T, V>* temp = head;
+        Node<T, V>* prev = NULL;
+        while (temp != NULL) {
+            if (temp->key == key) {
+                if (temp == head) {
+                    head = head->next;
+                } else {
+                    prev->next = temp->next;
+                }
+                delete temp;
+                return;
             }
-            return s;
+            prev = temp;
+            temp = temp->next;
         }
-    } 
+    }
 
-    void clear(){ // Elimina todos los elementos de la lista
-        // Si head es NULL ya la lista esta vacia asi que no se hace nada
-        while(head != NULL){
-            Node<T,V>* temp = head;
+    bool empty() {
+        return head == NULL;
+    }
+
+    int size() {
+        int s = 0;
+        Node<T, V>* temp = head;
+        while (temp != NULL) {
+            s++;
+            temp = temp->next;
+        }
+        return s;
+    }
+
+    void clear() {
+        while (head != NULL) {
+            Node<T, V>* temp = head;
             head = head->next;
             delete temp;
         }
     }
 
-    Node<T,V>* gethead(){
+    Node<T, V>* gethead() {
         return head;
     }
-
-
-
 };
 
-
 template <typename T, typename V>
-class TablaHash{
-    int m=MAX_SIZE;
-    LinkedList<T,V> LlistArray[MAX_SIZE];
+class TablaHash {
+    int m = MAX_SIZE;
+    LinkedList<T, V>* LlistArray; //cambiar a puntero dinamico
     int nelements;
-    int P=97;
-    int maxCol=3;
+    int P = 97;
+    int maxCol = 3;
     float fillfactor;
     int a;
     int b;
-    
 
-    public:
-    // Constructor
+public:
     TablaHash() : nelements(0) {
-        a  = getRandomNumber(1,P-1);
-        b = getRandomNumber(0,P-1);
+        LlistArray = new LinkedList<T, V>[m];
+        a = getRandomNumber(1, P - 1);
+        b = getRandomNumber(0, P - 1);
+    }
 
+    ~TablaHash() {
+        delete[] LlistArray;
     }
 
     int getRandomNumber(int lower, int P) {
@@ -192,111 +131,106 @@ class TablaHash{
         return distrib(gen);
     }
 
-    int newkey(T key){
-        int nkey;
+    int newkey(T key) {
+        int nkey = 0;
         if constexpr (std::is_same_v<T, std::string>) {
-            
-            // Tenemos 
-            nkey = 0;
-            int size=0;
             for (char c : key) {
-                size++;
+                nkey = nkey * 31 + c;
             }
+        } else if constexpr (std::is_same_v<T, char> || std::is_same_v<T, int>) {
+            nkey = static_cast<int>(key);
+        } else if constexpr (std::is_same_v<T, float>) {
+            nkey = static_cast<int>(key * 1000);
+        } else {
+            throw invalid_argument("esta variable no esta siendo manejada");
+        }
+        return nkey;
+    }
 
-            for (char c:key)
-            {
-                nkey+=(static_cast<int>(c))*static_cast<int>(pow(2,size-1));
-                size--;
-            }
-            return nkey;
+    int hashingfunction(T key) {
+        int nkey = newkey(key);
+        return ((a * nkey + b) % P) % m;
+    }
 
-        } else if constexpr (std::is_same_v<T, char>) {
-
-            nkey=static_cast<int>(key);
-            return nkey;
-
-        } else if constexpr (std::is_same_v<T, int>) {
-            nkey=key;
-            return nkey;
-
-        }  else {
-        cout << "This type of variable is not considered" << endl;
-        return -1;
+    void insert(T key, V value) {
+        int index = hashingfunction(key);
+        if (LlistArray[index].size() >= maxCol) {
+            reHash();
+            index = hashingfunction(key);
         }
 
-    }
+        int i = LlistArray[index].push_front(key, value);
+        nelements += i;
 
-    int hashingfunction(T key){
-        int nkey=newkey(key);
-        int index;
-        index=(( a*nkey+b ) % P ) % m;
-        return index;
-    }
-
-    void insert(T key, V value){ //ya actualiza
-        int index;
-        index=hashingfunction(key);
-        if(LlistArray[index].size()>=maxCol){
-            cout<<"Index lleno, excediste las colisiones permitidas"<<endl;
-        }else{
-            int i=LlistArray[index].push_front(key,value); //que pasa en actualizacion y como chequeo si llego al maximo de profundidad?
-            nelements+=i;// Si encontro el key i es 0, si no lo encontro i es 1
-            fillfactor=nelements/(maxCol*m);
-            if(fillfactor<0.5){
-            //rehash 
-            }
-
-        }
-        
-    }
-
-    V search(T key){
-        int index;
-        V value;
-        index=hashingfunction(key);
-        value=LlistArray[index].findKey(key);
-        return value;
-    }
-
-    bool empty(){
-        if(nelements==0){
-            return true;
-        }else{
-            return false;
+        fillfactor = static_cast<float>(nelements) / (maxCol * m);
+        if (fillfactor > 0.5) {
+            reHash();
         }
     }
 
-    void Delete(T key){
-        // Tengo que verificar si esta en la lista? porque creo que no seria necesario
-        int index;
-        index=hashingfunction(key);
+    V search(T key) {
+        int index = hashingfunction(key);
+        return LlistArray[index].findKey(key);
+    }
+
+    void Delete(T key) {
+        int index = hashingfunction(key);
         LlistArray[index].deleteKey(key);
-
     }
 
-    void showLists(int key){
-        int index;
-        index=hashingfunction(key);
-        Node<T,V>* head=LlistArray[index].gethead();
+    void reHash() {
+        int new_size = m * 2;
+        LinkedList<T, V>* new_list_array = new LinkedList<T, V>[new_size];
 
-        Node<T,V>* temp=head;
-        while(temp!=NULL){
-            cout<<"\n"<<endl;
-            cout<<"Key: "<<head->key<<endl;
-            cout<<"Value: "<<head->value<<endl;
-            temp=temp->next;
+        LinkedList<T, V>* old_list_array = LlistArray;
+        int old_size = m;
+
+        m = new_size;
+        LlistArray = new_list_array;
+
+        a = getRandomNumber(1, P - 1);
+        b = getRandomNumber(0, P - 1);
+
+        for (int i = 0; i < old_size; ++i) {
+            Node<T, V>* temp = old_list_array[i].gethead();
+            while (temp != NULL) {
+                int new_index = hashingfunction(temp->key);
+                LlistArray[new_index].push_front(temp->key, temp->value);
+                temp = temp->next;
+            }
         }
-        
+
+        delete[] old_list_array;
     }
-    
 };
 
+int main() {
+    TablaHash<int, string> tabla;
 
+    tabla.insert(1, "Valor1");
+    tabla.insert(2, "Valor2");
+    tabla.insert(3, "Valor3");
+    tabla.insert(4, "Valor4");
+    tabla.insert(5, "Valor5");
 
-int main(){
+    cout << "Clave 1: " << tabla.search(1) << endl;
+    cout << "Clave 2: " << tabla.search(2) << endl;
+    cout << "Clave 3: " << tabla.search(3) << endl;
+    cout << "Clave 4: " << tabla.search(4) << endl;
+    cout << "Clave 5: " << tabla.search(5) << endl;
 
-    TablaHash<char,string> Hash;
+    cout << "Clave 6 (no existente): " << tabla.search(6) << endl;
 
-    Hash.insert('a',".-");
-    
+    tabla.insert(1, "NuevoValor1");
+    tabla.insert(5, "NuevoValor5");
+
+    cout << "\nActualizando valores de clave 1 y clave 5:" << endl;
+    cout << "Clave 1 (actualizada): " << tabla.search(1) << endl;
+    cout << "Clave 5 (actualizada): " << tabla.search(5) << endl;
+
+    tabla.Delete(3);
+    cout << "\nEliminando clave 3..." << endl;
+    cout << "Clave 3 (después de eliminar): " << tabla.search(3) << endl;
+
+    return 0;
 }
